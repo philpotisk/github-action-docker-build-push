106c78f15111020f2fe8648fce0f5538973d3709
Philipp Potisk <philipp.potisk@danubetech.com>
2019-10-11 11:54:42 +0200

    Listing GitHub env-vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,45 @@
 #!/bin/sh -l
+set -eu
 
 echo "DOCKER BUILD AND PUSH"
-
-if [ -z "${DOCKER_FILE}" ]
-then
-   DOCKER_FILE=Dockerfile
-fi
 
 echo "Environment: "
 echo "- BUILD_PATH: ${BUILD_PATH}";
 echo "- DOCKER_FILE: ${DOCKER_FILE}";
 echo "- CONTAINER_TAG: ${CONTAINER_TAG}";
+echo "- GITHUB_TOKEN: ${GITHUB_TOKEN}";
+echo "- GITHUB_EVENT_NAME: ${GITHUB_EVENT_NAME}";
+echo "- GITHUB_EVENT_PATH: ${GITHUB_EVENT_PATH}";
+echo "- GITHUB_REF: ${GITHUB_REF}";
+echo "- GITHUB_WORKSPACE: ${GITHUB_WORKSPACE}";
+echo "- GITHUB_REPOSITORY: ${GITHUB_REPOSITORY}";
+echo "- GITHUB_ACTOR: ${GITHUB_ACTOR}";
 
+if [ -z "${DOCKER_FILE}" ]
+then
+   echo "Do Dockerfile spectified. Using default file: Dockerfile"
+   DOCKER_FILE=Dockerfile
+fi
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  echo "Set the GITHUB_TOKEN env variable."
+  exit 1
+fi
+
+if [[ -z "$GITHUB_EVENT_NAME" ]]; then
+  echo "Set the GITHUB_EVENT_NAME env variable."
+  exit 1
+fi
+
+if [[ -z "$GITHUB_EVENT_PATH" ]]; then
+  echo "Set the GITHUB_EVENT_PATH env variable."
+  exit 1
+fi
+GITHUB_REF
+if [[ -z "$GITHUB_REF" ]]; then
+  echo "Set the GITHUB_REF env variable."
+  exit 1
+fi
 
 ls -al
 
